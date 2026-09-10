82e6dcf8aa4d7dc78afc9a40b324dc94113c8007
hyungjun-dev <alextwin2@naver.com>
2026-09-10 22:55:25 +0900

    update: 관리자 라우트 코드 수정

--- src/api/manage/manage.routes.js
+++ src/api/manage/manage.routes.js
@@ -9,7 +9,7 @@ const router = express.Router();
  * @tag Manage
  * @summary Add a cleaning log entry for one of the manager's toilets
  * @req body { cleaning_type: number }
- * @res 200 { success: true }
+ * @res 201 { success: true }
  * @res 400 MISSING_REQUIRED_FIELDS
  * @res 401
  * @res 403 NO_PERMISSION
