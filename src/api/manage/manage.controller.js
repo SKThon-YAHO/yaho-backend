2cfe5085baa18b3d25edefb1c20f8cf30838da61
hyungjun-dev <alextwin2@naver.com>
2026-09-11 23:21:06 +0900

    update: is_valid 변수 로직 추가

--- src/api/manage/manage.controller.js
+++ src/api/manage/manage.controller.js
@@ -13,6 +13,30 @@ const addCleaningLog = async (req, res, next) => {
             throw error;
         }
 
+        let is_valid = false;
+        
+        for (const category of ['clean', 'break', 'item']) {
+            const group = cleaning_type[category];
+            
+            if (!group) continue;
+ 
+            for (const key in group) {
+                if (group[key] === true) {
+                    is_valid = true;
+                    break;
+                }
+            }
+ 
+            if (is_valid) break;
+        }
+
+        if (!is_valid) {
+            const error = new Error('Missing required fields.');
+            error.status = 400;
+            error.code = 'MISSING_REQUIRED_FIELDS';
+            throw error;
+        }
+
         await manageService.addCleaningLog({ local_code, toilet_code, cleaning_type });
 
         return res.status(201).json({ success: true });
