57c591b3a6e6a25a1e2068037402c12c0c65f488
nnxi <skwogur03@gmail.com>
2026-09-12 02:01:25 +0900

    update: 에러 로거

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,25 @@
 import logger from '../utils/logger.js';
 
 const errorHandler = (err, req, res, next) => {
+
+    console.log('========== ERROR HANDLER ==========');
+    console.log('Error:', err);
+    console.log('Error Message:', err.message);
+    console.log('Error Stack:', err.stack);
+    console.log('Error Status:', err.status);
+    console.log('Error Code:', err.code);
+
+    console.log('---------- Request ----ㄴ------');
+    console.log('Method:', req.method);
+    console.log('URL:', req.originalUrl);
+    console.log('Path:', req.path);
+    console.log('Params:', req.params);
+    console.log('Query:', req.query);
+    console.log('Body:', req.body);
+    console.log('Headers:', req.headers);
+
+    console.log('===================================');
+
     const status = err.status || 500;
     const code = err.code || 'INTERNAL_SERVER_ERROR';
 
